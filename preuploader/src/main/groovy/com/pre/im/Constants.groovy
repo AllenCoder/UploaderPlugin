5486a0263859dacc1b4f6458ff1527094e1742ce
Allen <AllenCoder@126.com>
2016-09-10 21:18:50 +0800

    commit
     commit

---
+++ preuploader/src/main/groovy/com/pre/im/Constants.groovy
@@ -0,0 +1,17 @@
+package com.pre.im
+
+class Constants {
+    public static final String HTTPMETHOD_GET = "GET"
+    public static final String HTTPMETHOD_POST = "POST"
+    public static final String HTTPMETHOD_PUT = "PUT"
+
+    public static final String USER_KEY = "user_key"
+    public static final String FILE = "file"
+    public static final String PASSWORD = "password"
+    public static final String UPDATE_NOTIFY = "update_notify"
+    public Boolean enable = true // 插件开关
+    public Boolean autoUpload = false // 是否自动上传
+    public Boolean debugOn = false // debug模式是否上传
+
+
+}
